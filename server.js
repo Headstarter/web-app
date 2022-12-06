660e71cd1f37eabe0bfffe16325332de16a0c2de
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-12-06 19:10:31 +0100

    deploy landing page

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const port = process.env.PORT || 8888;
 const express = require('express');
 var app = express();
 
-const db = require('./utils/database');
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -16,22 +14,7 @@ app.use('/static', express.static('public'));
 // index page
 app.get('/', async function(req, res) {
   res.render('pages/index', {
-    page_title: "Homepage / Headstarter",
-    jobs: await db.jobs.find().map((job) => {
-      return {
-        '_id': job._id,
-        'name': job.name,
-        'category': 'Category',
-        'level': 'Level required'
-      };
-    }).toArray()
-  });
-});
-
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about', {
-    page_title: "About us / Headstarter"
+    page_title: "Очаквайте Headstarter скоро"
   });
 });
 
