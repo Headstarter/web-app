5361ed9970e648dc7ef0b95eb684cde1b922c538
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2022-12-16 09:27:38 +0100

    Redesign & image server-side processing
    - Redesigning call-to-action
    - Adding jQuery
    - Add dimensioning on all images

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#subscribe-with-email").submit(function (e) {
+    $(".subscribe-with-email").submit(function (e) {
         //stop submitting the form 
         e.preventDefault();
         return true;
